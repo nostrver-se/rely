279a0bef17839315935b875f335fe416b833914a
pippellia-btc <pippellia@gmail.com>
2026-02-11 12:11:36 +0100

    moved ApplyBudget to processor

--- processor.go
+++ processor.go
@@ -1,5 +1,7 @@
 package rely
 
+import "github.com/nbd-wtf/go-nostr"
+
 type processor struct {
 	maxWorkers int
 	queue      chan request
@@ -91,5 +93,79 @@ func (p *processor) Process(request request) {
 		}
 
 		request.client.send(countResponse{ID: ID, Count: count, Approx: approx})
+	}
+}
+
+// ApplyBudget adjusts the Limit of each filter in-place so that the total does not exceed the given budget.
+// Filters with limits <= budget / len(filters) are preserved, while larger ones are scaled down proportionally.
+// It panics if budget is negative.
+func ApplyBudget(budget int, filters ...nostr.Filter) {
+	if budget < 0 {
+		panic("rely.ApplyBudget: budget should not be negative")
+	}
+
+	if len(filters) == 0 {
+		return
+	}
+
+	if budget < len(filters) {
+		// give 1 to as many filters as possible, set the rest to zero
+		for i := range filters {
+			if i < budget {
+				filters[i].Limit = 1
+			} else {
+				filters[i].Limit = 0
+				filters[i].LimitZero = true
+			}
+		}
+		return
+	}
+
+	used := 0
+	for i := range filters {
+		if filters[i].LimitZero {
+			// ensure consistency
+			filters[i].Limit = 0
+		}
+
+		if !filters[i].LimitZero && filters[i].Limit < 1 {
+			// limit is unspecified (or negative), so we set it equal to the budget
+			filters[i].Limit = budget
+		}
+
+		used += filters[i].Limit
+	}
+
+	if used > budget {
+		// modify filters based on whether they have a limit lower or higher than budget / len(filters).
+		// 	- lowers: do nothing
+		//	- highers: linearly scale their limit
+
+		fair := budget / len(filters)
+		var sumHighers int
+		var highers []int
+
+		for i := range filters {
+			limit := filters[i].Limit
+			if limit > fair {
+				highers = append(highers, i)
+				sumHighers += limit
+			} else {
+				budget -= limit
+			}
+		}
+
+		scalingFactor := float64(budget) / float64(sumHighers)
+		for _, idx := range highers {
+			limit := float64(filters[idx].Limit)
+			newLimit := int(scalingFactor * limit)
+
+			if newLimit == 0 {
+				filters[idx].Limit = 0
+				filters[idx].LimitZero = true
+			} else {
+				filters[idx].Limit = newLimit
+			}
+		}
 	}
 }
